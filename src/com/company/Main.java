c62e9fabe994bdc86c95496b5746db0cd90bb885
rajnij06 <jangir.r@northeastern.edu>
2021-09-16 20:02:21 -0700

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    System.out.println("Hello World");
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        max = max+1;
+        min = min - 1;
+
+        System.out.println("Max value = " + max);
+        short maxS = Short.MAX_VALUE;
+        short minS = Short.MIN_VALUE;
+
+        System.out.println("Max value = " + maxS);
+        float myFloat = 6.6f;
+        float maxF = Float.MAX_VALUE;
+        float minF = Float.MIN_VALUE;
+        System.out.println("Max value = " + maxF);
+        System.out.println("Min value = " + minF);
+
+        char ch = '\u00A9';
+        System.out.println(ch);
+
+
+    }
+}
